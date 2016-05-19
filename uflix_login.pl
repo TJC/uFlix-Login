367d6087880f812276e3a2bdfcd9b9b5618870dd
Toby Corkindale <tjc@wintrmute.net>
2016-05-19 12:04:12 +1000

    Script now correctly updates IP when required

--- uflix_login.pl
+++ uflix_login.pl
@@ -36,4 +36,54 @@ if ($r->code != 200) {
     exit 1;
 }
 
+if (is_ip_valid($r)) {
+    exit 0;
+}
+
+my $xfToken = find_xftoken($r->content);
+$r = $ua->post("https://uflix.com.au/u/my/auth-ip",
+    Content => {
+        submit => "Check and Update",
+        "_xfToken" => $xfToken,
+    },
+);
+if ($r->code == 303) {
+    my $next_hop = $r->header("Location");
+    $r = $ua->get($next_hop);
+}
+
+if ($r->code != 200) {
+    say "Something went wrong.";
+    say $r->status_line;
+    say $r->content;
+    exit 1;
+}
+
 exit 0;
+
+
+# Ugly regex to find hidden input value; should really do HTML parsing properly..
+sub find_xftoken {
+    my $content = shift;
+    my ($token) = $content =~ /input type="hidden" name="_xfToken" value="([a-f\d,]+)"/;
+    die "Could not find xfToken value" unless (length $token);
+    return $token;
+}
+
+sub is_ip_valid {
+    my $r = shift;
+    return not ($r->content =~ /is outdated. Please/);
+}
+
+# The site looks like it uses an ajax query to find out if the IP is currently valid.
+# The code below *should* work, but doesn't :(
+sub ajax_check {
+    my $xfToken = find_xftoken($r->content);
+    my $r = $ua->post("https://uflix.com.au/u/my/auth-ip?_xfNoRedirect=1&_xfToken=$xfToken&_xfRequestUri=/u/my/auth-ip",
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+    );
+    say $r->status_line;
+    say $r->headers->as_string;
+    say $r->content;
+}
